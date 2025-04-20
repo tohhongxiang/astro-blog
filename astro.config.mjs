a61f84812449a852a7315a5494fe68b005fd9b31
Toh Hong Xiang <tohhongxiang@gmail.com>
2025-04-20 17:03:35 +0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [react()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	markdown: {
+		shikiConfig: {
+			theme: "dracula",
+		},
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [
+			[
+				rehypeKatex,
+				{
+					// Katex plugin options
+				},
+			],
+		],
+	},
+});
---
+++ src/pages/blog/post-2/index.md
@@ -0,0 +1,13 @@
+---
+layout: "@/layouts/blog-layout.astro"
+title: Lmao
+date: 2025-04-02
+---
+
+# Hello world
+
+```ts
+export default function getMessage(a: string, b: string) {
+	return a + b + "hello world!";
+}
+```
